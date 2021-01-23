9d38fe8e0f4af51c9979cd750ce99224bac95362
Chris Briant <cbriant@btinternet.com>
2021-01-23 18:04:13 +0000

    I have got the client removal on disconnect working

--- server3.py
+++ server3.py
@@ -1,4 +1,4 @@
-import sys, redis, uuid,json,ast
+import sys, redis, uuid,json,ast, threading
 
 from twisted.internet import reactor, ssl
 from twisted.web.server import Site
@@ -45,6 +45,8 @@ class BroadcastServerProtocol(WebSocketServerProtocol):
                 self.factory.send_client(received_data['client_id'],json.dumps(send_payload))
                 #Broadcast client list now a name is set
                 self.factory.send_client_list()
+            elif received_data['type'] == 'enter_room':
+                self.factory.send_room(enter_room,received_data['client_id'],received_data['name'])
 
     def connectionLost(self, reason):
         print("Connection Lost")
@@ -57,6 +59,7 @@ class BroadcastServerFactory(WebSocketServerFactory):
         WebSocketServerFactory.__init__(self, url)
         self.clients = {}
         self.rooms = {}
+        self.timers = {}
 
     def register(self, client):
         # ids = list(self.clients.keys())
@@ -97,10 +100,14 @@ class BroadcastServerFactory(WebSocketServerFactory):
             except Exception as e:
                 print(e)
 
+    #The connection is closed tidy up
     def unregister(self, client):
-        if client in self.clients:
-            print("unregistered client {}".format(client.peer))
-            self.clients.remove(client)
+        all_clients = list(self.clients)
+        for cli in all_clients:
+            if client == self.clients[cli]['client']:
+                del self.clients[cli]
+        bojo = list(self.clients)
+        
 
     def broadcast(self, msg):
         # print("broadcasting message '{}' to {} clients ...".format(msg, len(self.clients)))
@@ -128,6 +135,9 @@ class BroadcastServerFactory(WebSocketServerFactory):
             self.rooms[room]['name'] = room
             self.rooms[room]['members'] = []
             self.rooms[room]['members'].append(client_id)
+            self.timers[room] = dict()
+            self.timers[room]['timer'] = threading.Timer(10,self.close_room,args=room)
+            self.timers[room]['timer'].start()
             cids = self.clients.keys()
             room_list = [ self.rooms[k] for k in self.rooms.keys()]
             send_payload = {
@@ -147,7 +157,27 @@ class BroadcastServerFactory(WebSocketServerFactory):
             }
             self.clients[client_id]['client'].sendMessage(json.dumps(send_payload).encode('utf-8'))
 
+    def enter_room(client_id,room_name):
+        room = self.rooms['room_name']
+        if client_id not in room['members']:
+            room['members'].append(client_id)
+            send_payload = {
+                'type' : 'room_entrance',
+                'client': { 'id':client_id, 'name':self.clients[client_id]['name']}
+            }
+            send_room(room,send_payload)
 
+    #Send data to a room
+    def send_room(room,payload):
+        for cid in room['members']:
+            try:
+                self.clients[cid]['client'].sendMessage(json.dumps(send_payload).encode('utf-8'))
+            except Exception as e:
+                print(e)
+
+
+    def close_room(self,room):
+        print('Closing Room: ', room)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -4,20 +4,50 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-import sys
+import sys, random, string
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(100)
+
 
 def test_set_name():
     # chrome_options = Options()
     # chrome_options.add_experimental_option("detach", True)
     # driver = webdriver.Chrome(options=chrome_options)
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(100)
+
     driver.execute_script('window.open("");')
     driver.switch_to_window(driver.window_handles[-1])
     driver.get('http://localhost:3000/')
+
+def test_rooms():
+    #Open four sessions
+    for i in range(0,3):
+        driver.execute_script('window.open("");')
+        driver.get('http://localhost:3000/')
+        letters = string.ascii_letters
+        name = ''.join(random.choice(letters) for i in range(6))
+        driver.find_element_by_id("name").send_keys(name)
+        driver.find_element_by_id("sendname").click()
+        driver.switch_to_window(driver.window_handles[-1])
+    driver.get('http://localhost:3000/')
+    letters = string.ascii_letters
+    name = ''.join(random.choice(letters) for i in range(6))
+    driver.find_element_by_id("name").send_keys(name)
+    driver.find_element_by_id("sendname").click()
+
+    #Create two rooms
+    driver.switch_to_window(driver.window_handles[0])
+    driver.find_element_by_id("room-name").send_keys('Room A')
+    #driver.find_element_by_id("sendroom").click()
+    driver.switch_to_window(driver.window_handles[2])
+    driver.find_element_by_id("room-name").send_keys('Room B')
+
+
 
 
 if __name__ == '__main__':
     print ('Argument List:', str(sys.argv))
     if sys.argv[1] == 'set_name':
         test_set_name()
+    elif sys.argv[1] == 'test_rooms':
+        test_rooms()
